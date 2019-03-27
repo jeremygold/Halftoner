4abeac76657c06e4bdf92e4bcc8f4d4e090640e7
Chris Molloy <chris.molloy@protonmail.com>
2019-03-27 15:53:42 +1300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Halftoner
+# Python Hex-grid Halftoner
+I couldn't find Python halftoner that worked on a hexagonal grid pattern (there are several that work on an orthogonal grid), so I wrote my own.
+
+The code uses [https://opencv.org/](OpenCV) to do the heavy lifting. I used https://hub.docker.com/r/yoanlin/opencv-python3
+
+<pre>
+usage: app.py [-h] -i INPUT -o OUTPUT [-r RADIUS] [-t THRESHOLD]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        path to the input image
+  -o OUTPUT, --output OUTPUT
+                        path to the output image (PNG, JPG or SVG)
+  -r RADIUS, --radius RADIUS
+                        grid radius, in pixels (default 10)
+  -t THRESHOLD, --threshold THRESHOLD
+                        dots smaller than this are not drawn in output
+                        (default 0)
+
+e.g.: $> python3 halftoner.py -i skull.jpg -o skull.svg -r 10 -t 2
+</pre>
+
+For more information, see https://chrismolloy.com/halftoner
+
+![Sample output](skull.png)
+
---
+++ halftoner.py
@@ -0,0 +1,83 @@
+# //////////////////////////////////////////////////////////////////////////////
+# //  
+# //  'Hex Halftoner' by Chris Molloy (http://chrismolloy.com/halftoner)
+# //  
+# //  Released under Creative Commons Attribution Share Alike 4.0 International
+# //  
+# //////////////////////////////////////////////////////////////////////////////
+
+#!/usr/bin/python3
+
+import argparse
+import cv2
+import math
+import numpy as np
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--input', required=True, help='path to the input image')
+ap.add_argument('-o', '--output', required=True, help='path to the output image (PNG, JPG or SVG)')
+ap.add_argument('-r', '--radius', type=int, default=10, help='grid radius, in pixels (default 10)')
+ap.add_argument('-t', '--threshold', type=int, default=0, help='dots smaller than this are not drawn in output (default 0)')
+args = vars(ap.parse_args())
+
+greyscale = cv2.cvtColor(cv2.imread(args['input']), cv2.COLOR_BGR2GRAY)
+outFile = args['output']
+outRadius = int(args['radius'])
+threshold = int(args['threshold'])
+
+isSVG = (outFile[-4:].lower() == '.svg')
+inRadius = math.ceil(math.sqrt(3 / 4) * outRadius)
+diameter = (outRadius * 2) + 1 # pseudo-diameter (always odd, as per convolution kernel reqs)
+
+circleMask = np.zeros((diameter, diameter), dtype='float32')
+cv2.circle(circleMask, (outRadius, outRadius), outRadius, (1, 1, 1), -1) # not sure why the colour here needs to be (1, 1, 1), as oppsed to (255, 255, 255) - scaling?
+circleMask = circleMask * (1.0 / np.count_nonzero(circleMask))
+
+convolved = cv2.filter2D(greyscale, -1, circleMask)
+
+(iH, iW) = convolved.shape[:2]
+countX = math.floor(iW / (outRadius * 3.0))
+countY = math.floor(iH / inRadius)
+
+if isSVG:
+    sSVG = []
+    sSVG.append('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>')
+    sSVG.append('<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">')
+    sSVG.append('<svg viewBox="0 0 ' + str(iW) + ' ' + str(iH) + '" height="' + str(iH) + 'mm" width="' + str(iW) + 'mm" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">')
+    sSVG.append('<title>Hex Halftoner</title>')
+    sSVG.append('<desc>Author: Chris Molloy. License: Creative Commons Attribution Share Alike 4.0 International. For more information, see https://chrismolloy.com/halftoner</desc>')
+    sSVG.append('<g id="docRoot" style="overflow:hidden;">')
+    sSVG.append('<rect id="bg" height="100%" width="100%" fill="black"/>')
+else:
+    outImage = np.zeros((iH, iW), dtype='float32') # all black
+# end if
+
+ii = 0 # circle id index, prefixed with 'c'
+for yy in range(0, countY):
+    iY = math.floor(yy * inRadius)
+    for xx in range(0, countX):
+        iX = math.floor((((xx * 3.0) + 1.0) * outRadius) + (0 if (yy % 2 == 0) else (1.5 * outRadius)))
+        iC = convolved[iY, iX]
+        if iC > 0: # ignore black
+            rr = ((convolved[iY, iX] / 255.0) * inRadius) - 1.0 # 1px spacer
+            if rr > threshold: # ignore small
+                if isSVG:
+                    sSVG.append('<circle id="c' + '{:04d}'.format(ii) + '" cx="' + str(iX) + '" cy="' + str(iY) + '" r="' + '{0:.2f}'.format(rr) + '" stroke="none" fill="white" />')
+                    ii += 1
+                else:
+                    cv2.circle(outImage, (iX, iY), math.floor(rr), (255, 255, 255), -1) # white circle
+                # end if
+            # end if
+        # end if
+    # end for xx
+# end for yy
+
+if isSVG:
+    sSVG.append('</g>')
+    sSVG.append('</svg>')
+    file_out = open(outFile, 'w')
+    file_out.write('\n'.join(sSVG))
+    file_out.close()
+else:
+    cv2.imwrite('out.png', outImage)
+# end if
